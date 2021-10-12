ce1d16f896c10cf4f4b0ddbd6c8a4e4f50d4370a
PCC <juangpochieh@gmail.com>
2021-10-12 10:28:21 +0800

    feat: add floating button

---
+++ Publisher/HomePageCell.swift
@@ -0,0 +1,23 @@
+//
+//  HomePageCell.swift
+//  Publisher
+//
+//  Created by PoChieh Chuang on 2021/10/12.
+//
+
+import UIKit
+
+class HomePageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Publisher/ViewController.swift
+++ Publisher/ViewController.swift
@@ -9,12 +9,112 @@ import UIKit
 import Firebase
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var floatingButton: UIButton?
+    private let floatingButtonImageName = "Icons_24px_Add01"
+    private static let buttonHeight: CGFloat = 75.0
+    private static let buttonWidth: CGFloat = 75.0
+    private let roundValue = ViewController.buttonHeight/2
+    private let trailingValue: CGFloat = 15.0
+    private let leadingValue: CGFloat = 15.0
+    private let shadowRadius: CGFloat = 2.0
+    private let shadowOpacity: Float = 0.5
+    private let shadowOffset = CGSize(width: 0.0, height: 5.0)
+    private let scaleKeyPath = "scale"
+    private let animationKeyPath = "transform.scale"
+    private let animationDuration: CFTimeInterval = 0.4
+    private let animateFromValue: CGFloat = 1.00
+    private let animateToValue: CGFloat = 1.05
+    
+    func addData() {
+        let articles = Firestore.firestore().collection("articles")
+        let document = articles.document()
+        let data: [String: Any] = [
+            "author": [
+                "email": "wayne@school.appworks.tw",
+                "id": "waynechen323",
+                "name": "AKA小安老師"
+            ],
+            "title": "IU「亂穿」竟美出新境界！笑稱自己品味奇怪　網笑：靠顏值撐住女神氣場",
+            "content": "南韓歌手IU（李知恩）無論在歌唱方面或是近期的戲劇作品都有亮眼的成績，但俗話說人無完美、美玉微瑕，曾再跟工作人員的互動影片中坦言自己品味很奇怪，近日在IG上分享了宛如「媽媽們青春時代的玉女歌手」超復古穿搭造型，卻意外美出新境界。",
+            "createdTime": NSDate().timeIntervalSince1970,
+            "id": document.documentID,
+            "category": "Beauty"
+        ]
+        document.setData(data)
     }
 
+    public override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        createFloatingButton()
+    }
 
+    public override func viewWillDisappear(_ animated: Bool) {
+        guard floatingButton?.superview != nil else {  return }
+        DispatchQueue.main.async {
+            self.floatingButton?.removeFromSuperview()
+            self.floatingButton = nil
+        }
+        super.viewWillDisappear(animated)
+    }
+
+    private func createFloatingButton() {
+        floatingButton = UIButton(type: .custom)
+        floatingButton?.translatesAutoresizingMaskIntoConstraints = false
+        floatingButton?.backgroundColor = .white
+        floatingButton?.setImage(UIImage(named: floatingButtonImageName), for: .normal)
+        floatingButton?.addTarget(self, action: #selector(doThisWhenButtonIsTapped(_:)), for: .touchUpInside)
+        constrainFloatingButtonToWindow()
+        makeFloatingButtonRound()
+        addShadowToFloatingButton()
+        addScaleAnimationToFloatingButton()
+    }
+
+    // TODO: Add some logic for when the button is tapped.
+    @IBAction private func doThisWhenButtonIsTapped(_ sender: Any) {
+        print("Button Tapped")
+    }
+
+    private func constrainFloatingButtonToWindow() {
+        DispatchQueue.main.async {
+            guard let keyWindow = UIApplication.shared.keyWindow,
+                let floatingButton = self.floatingButton else { return }
+            keyWindow.addSubview(floatingButton)
+            keyWindow.trailingAnchor.constraint(equalTo: floatingButton.trailingAnchor,
+                                                constant: self.trailingValue).isActive = true
+            keyWindow.bottomAnchor.constraint(equalTo: floatingButton.bottomAnchor,
+                                              constant: self.leadingValue).isActive = true
+            floatingButton.widthAnchor.constraint(equalToConstant:
+                ViewController.buttonWidth).isActive = true
+            floatingButton.heightAnchor.constraint(equalToConstant:
+                ViewController.buttonHeight).isActive = true
+        }
+    }
+
+    private func makeFloatingButtonRound() {
+        floatingButton?.layer.cornerRadius = roundValue
+    }
+
+    private func addShadowToFloatingButton() {
+        floatingButton?.layer.shadowColor = UIColor.black.cgColor
+        floatingButton?.layer.shadowOffset = shadowOffset
+        floatingButton?.layer.masksToBounds = false
+        floatingButton?.layer.shadowRadius = shadowRadius
+        floatingButton?.layer.shadowOpacity = shadowOpacity
+    }
+
+    private func addScaleAnimationToFloatingButton() {
+        // Add a pulsing animation to draw attention to button:
+        DispatchQueue.main.async {
+            let scaleAnimation: CABasicAnimation = CABasicAnimation(keyPath: self.animationKeyPath)
+            scaleAnimation.duration = self.animationDuration
+            scaleAnimation.repeatCount = .greatestFiniteMagnitude
+            scaleAnimation.autoreverses = true
+            scaleAnimation.fromValue = self.animateFromValue
+            scaleAnimation.toValue = self.animateToValue
+            self.floatingButton?.layer.add(scaleAnimation, forKey: self.scaleKeyPath)
+        }
+    }
 }
-
